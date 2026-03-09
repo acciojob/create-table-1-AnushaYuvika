bb96583fd5cce7206768bda993ca534999469779
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2026-03-09 17:53:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  const row = document.createElement("tr");
+  const tableData1 = document.createElement("td");
+  tableData1.textContent = "New Cell1";
+  const tableData2 = document.createElement("td");
+  tableData2.textContent = "New Cell2";
+
+  row.appendChild(tableData1);
+  row.appendChild(tableData2);
+	
+  const table = document.querySelector("#sampleTable");
+  table.insertBefore(row, table.children[1]);
+  table.prepend(row);
 }
+
